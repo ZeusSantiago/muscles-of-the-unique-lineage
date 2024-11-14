54c3fb1710258cd034c17d3e317855d81f7ea155
skatistics <131618960+skatistics@users.noreply.github.com>
2024-11-14 15:44:52 +0800

    modified LandingPage
    added UpdateRoutinesModal
    added RoutinesList

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,22 @@
-import AddWorkoutModal from "../components/modals/AddWorkoutModal.jsx";
-import UpdateWorkoutModal from "../components/modals/UpdateWorkoutModal.jsx";
+import RoutinesList from "../components/RoutinesList.jsx";
 import WorkoutList from "../components/WorkoutList.jsx";
 import { useState } from "react";
 
 const LandingPage = () => {
+  const [hidden, setHidden] = useState(true);
+
   const [currentWorkout, setCurrentWorkout] = useState({
     name: "",
     muscleGroup: "",
     workoutType: "",
-    intensity: 2,
+    intensity: 1,
   });
-  const [hidden, setHidden] = useState(true);
+  const [currentRoutine, setCurrentRoutine] = useState({
+    name: "",
+    intensity: 1,
+  });
+
+  const [routines, setRoutines] = useState([]);
   const [workouts, setWorkouts] = useState([]);
   const fetchData = () => {
     fetch("https://motul-backend.vercel.app/api/workouts/")
@@ -18,13 +24,28 @@ const LandingPage = () => {
       .then((res) => {
         setWorkouts(res);
       });
+    fetch("https://motul-backend.vercel.app/api/routines/")
+      .then((res) => res.json())
+      .then((res) => {
+        setRoutines(res);
+      });
   };
   return (
     <>
-      <div className="bg-red-700">
-        <button>Add Routine</button>
+      <div className="">
+        <RoutinesList
+          setCurrentRoutine={setCurrentRoutine}
+          setHidden={setHidden}
+          routines={routines}
+          fetchData={fetchData}
+        />
       </div>
-      <div>
+      <div className=" flex flex-col absolute right-12 top-24 ">
+        <button>Create a Group</button>
+        <button>Join a Group</button>
+        <button className="mt-8">Request Workout</button>
+      </div>
+      <div className="mt-40">
         <WorkoutList
           setCurrentWorkout={setCurrentWorkout}
           setHidden={setHidden}
